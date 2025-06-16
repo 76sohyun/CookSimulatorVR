936c2847b9cfb1b897fb1a77f20b77a056b5a4c0
강소현 <conut1119@gmail.com>
2025-06-16 22:02:15 +0900

    [Feat[ 소스짜기 구현

--- Assets/Scripts/FoodCut.cs
+++ Assets/Scripts/FoodCut.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChickenCut : MonoBehaviour
+public class FoodCut : MonoBehaviour
 {
     public GameObject[] chickenParts;
     private int hit;
